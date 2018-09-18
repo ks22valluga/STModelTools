80c6d6de87d5b37c07f3543fa4f7e2d78bca44af
kevins <kevin.stewart@passivsystems.com>
2018-09-18 13:43:49 +0100

    Making degree ymbol utf-8 compliant

--- src/com/ks22valluga/app/TextFieldCentigrade.java
+++ src/com/ks22valluga/app/TextFieldCentigrade.java
@@ -1,6 +1,6 @@
 package com.ks22valluga.app;
 
-import javax.swing.JTextField;;
+import javax.swing.JTextField;
 
 public class TextFieldCentigrade extends JTextField {
 
@@ -8,15 +8,15 @@ public class TextFieldCentigrade extends JTextField {
 	 * 
 	 */
 	private static final long serialVersionUID = -5073059353640189538L;
-
+    private static final String degreeSymb = "\u00b0";
 	@Override
 	public void setText(String text) {
-		super.setText(text+"°C");
+		super.setText(text+degreeSymb+"C");
 	}
 
 	@Override
 	public synchronized String getText() {
-		String retString = super.getText().replace("°C", "");
+		String retString = super.getText().replace( degreeSymb+"C", "");
 		return retString;
 	}
 	
