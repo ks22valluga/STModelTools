deb7ea602bcfced88cb6c2eeb4d7785186f1306c
ks22valluga@gmail.com <kevin.stewart@passivsystems.com>
2018-11-14 08:21:58 +0000

    added initial inter zone connector

---
+++ src/com/ks22valluga/examples/InterZoneConnector.java
@@ -0,0 +1,115 @@
+package com.ks22valluga.examples;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import com.ks22valluga.thermal.SimpleTEntity;
+
+public class InterZoneConnector {
+
+    private SimpleTEntity root;
+    private SimpleTEntity endA,endB;
+    private SimpleTEntity child11, child21, child31, child41, child51, child61;
+    private ArrayList<SimpleTEntity> chain = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+	InterZoneConnector zn = new InterZoneConnector();
+
+	// set zone temp 19.5 = 1950
+	for (SimpleTEntity ste : zn.chain) {
+	    ste.setTemp(0);
+	}
+	// set root to boiler temp 60deg
+	zn.root.setFixedTemp(4000f);
+
+	// set ext temp 2 deg
+	zn.child61.setFixedTemp(0f);
+	zn.child61.setMass(1900f);
+
+	// set mass on adjacent node
+
+
+	StringBuilder sb = new StringBuilder();
+	sb.append("Min,");
+	for (SimpleTEntity ste : zn.chain) {
+	    sb.append(ste.getFriendlyName() + ",");
+	}
+	System.out.println(sb.toString());
+
+	for (int i = 0; i < 10000; i++) {
+	    zn.root.update();
+	    if (i != 1500) {
+		sb = new StringBuilder();
+
+		sb.append(i + ",");
+		for (SimpleTEntity ste : zn.chain) {
+		    sb.append(ste.getTemp() + ",");
+		}
+		System.out.println(sb.toString());
+	    }
+
+	}
+
+	// zn.root.setFixedTemp(false);
+	//zn.root.setFixedTemp(zn.child31.getTemp());
+
+//	for (int i = 0; i < 541; i++) {
+//	  //  zn.root.setTemp(zn.child11.getTemp());
+//	    zn.root.update();
+//
+//	    if (i != 5400) {
+//		sb = new StringBuilder();
+//
+//		sb.append(i + ",");
+//		for (SimpleTEntity ste : zn.chain) {
+//		    sb.append(ste.getTemp() + ",");
+//		}
+//		System.out.println(sb.toString());
+//	    }
+
+	//}
+	
+	//System.out.println(zn.root.buildConfig().toString(2));
+
+    }
+
+    public InterZoneConnector() {
+	root = new SimpleTEntity(null, 500, 0, 5, "root");
+	chain.add(root);
+	float tempRoot = root.getTemp();
+
+	root.addChild(500, 0, 5, "child11");
+	child11 = root.getChildren()[0];
+	chain.add(child11);
+	float tempChild1 = child11.getTemp();
+
+	child11.addChild(500, 0, 5, "child21");
+	child21 = child11.getChildren()[0];
+	chain.add(child21);
+	float tempChild21 = child21.getTemp();
+
+	child21.addChild(500, 0, 5, "child31");
+	child31 = child21.getChildren()[0];
+	chain.add(child31);
+
+
+	child31.addChild(500, 0, 5, "child41");
+	child41 = child31.getChildren()[0];
+	chain.add(child41);
+	float tempChild41 = child41.getTemp();
+
+	child41.addChild(500, 0, 5, "child51");
+	child51 = child41.getChildren()[0];
+	chain.add(child51);
+	float tempChild51 = child51.getTemp();
+
+	child51.addChild(500, 0, 5, "child61");
+	child61 = child51.getChildren()[0];
+	chain.add(child61);
+	float tempChild61 = child61.getTemp();
+
+    }
+
+}
